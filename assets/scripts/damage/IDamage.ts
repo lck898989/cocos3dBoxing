c4081bd2b0eaf041b5f53265551c3b1dd4f7b7df
lck898989 <1783887127@qq.com>
2022-05-26 16:41:09 +0800

    feat: Add enemy death and other effect

--- assets/scripts/damage/IDamage.ts
+++ assets/scripts/damage/IDamage.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { _decorator, Component, Node, Vec3 } from 'cc';
-=======
-import { _decorator, Component, Node } from 'cc';
->>>>>>> 87a5db7 (feat: 添加伤害，技能碰撞检测代码)
 import { DamageObject } from './DamageObject';
 const { ccclass, property } = _decorator;
 
